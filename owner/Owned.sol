4c0495157a7817adb19569f19639782554a628ca
8kb <mendel@zzzlab.com>
2018-03-02 16:56:52 +0200

    Add files via upload

---
+++ owner/Owned.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+/**
+ * @title Owned
+ * @dev The Owned contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract OwnerOwned {
+    address public owner = msg.sender;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner
+     * @param newOwner The address to transfer ownership to
+     */
+    function transferOwnership(address newOwner) onlyOwner public {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
