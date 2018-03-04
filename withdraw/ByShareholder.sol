50c6d0ca8e699c7c0c351c1fa3097373f1efa2bd
8kb <mendel@zzzlab.com>
2018-03-04 17:06:06 +0200

    Update ByShareholder.sol

--- withdraw/ByShareholder.sol
+++ withdraw/ByShareholder.sol
@@ -1,13 +1,15 @@
 pragma solidity ^0.4.18;
+import "../DryContract.sol";
 import "../share/IShared.sol";
 
-contract WithdrawByShareholder is ShareIShared {
+contract WithdrawByShareholder is DryContract, ShareIShared {
     /**
      * @dev Withdraw all shared balance by shareholder 
      */
     function withdrawShare() public returns (bool) {
         require(shareCount() != 0);
-        uint256 amount = shareSize(msg.sender) * this.balance / shareCount();
+        uint256 balansedShare = shareSize(msg.sender).mul(this.balance); 
+        uint256 amount =  balansedShare.div(shareCount());
         require(amount != 0);
         shareTransfer(msg.sender, 0, shareSize(msg.sender));
         msg.sender.transfer(amount);
