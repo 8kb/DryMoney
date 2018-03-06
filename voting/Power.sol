2404426ccc04ac820f0dbb1e0872f055599228a7
8kb <mendel@zzzlab.com>
2018-03-06 22:04:15 +0200

    Update Power.sol

--- voting/Power.sol
+++ voting/Power.sol
@@ -16,7 +16,7 @@ contract VotingPower is ShareIShared {
     }
     
     function tryAdd(address user) private {
-        if((powers[user].number == 0) && (powers[_user].power >= powerMinimum)) {
+        if((powers[user].number == 0) && (powers[_\user].power >= powerMinimum)) {
             poweredUsers[poweredUsersCount] = user;
             poweredUsersCount++;
             powers[user].number = poweredUsersCount;
@@ -24,7 +24,7 @@ contract VotingPower is ShareIShared {
     }
     
     function tryDelete(address user) private {
-        if(powers[user].number > 0 && powers[_user].power < powerMinimum) {
+        if(powers[user].number > 0 && powers[user].power < powerMinimum) {
             uint64 userId = powers[user].number - 1;
             powers[user].number = 0;
             poweredUsersCount--;
@@ -41,10 +41,10 @@ contract VotingPower is ShareIShared {
     function powerTransfer(address _from, address _to, uint256 _value) internal returns (bool) {
         if(_from != address(0)) {
             require(_value <= powers[_from].power);
-            shares[_from] = powers[_from].power.sub(_value);
+            powers[_from] = powers[_from].power.sub(_value);
         }
         if(_to != address(0)) {
-            shares[_to] = powers[_to].power.add(_value);
+            powers[_to] = powers[_to].power.add(_value);
         }
         return true;
     }
