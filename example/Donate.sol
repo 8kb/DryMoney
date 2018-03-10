4e01e64a9287a097172aad2d08fe1845bddb2406
8kb <mendel@zzzlab.com>
2018-03-10 22:47:59 +0200

    Update Donate.sol

--- example/Donate.sol
+++ example/Donate.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.18;
 import "github.com/8kb/DryMoney/balance/WithdrawByOwnerLimited.sol";
-import "github.com/8kb/DryMoney/balance/RefillLimited.sol";
+import "github.com/8kb/DryMoney/balance/Limit.sol";
 
-contract Donate is BalanceWithdrawByOwnerLimited, BalanceRefillLimited
+contract Donate is BalanceWithdrawByOwnerLimited, BalanceLimit
 {
     function Donate() public {
         ownerLimitMaximum = 1 ether;
         ownerLimitSpeed = uint256(1 ether) / uint256(1 weeks);
     }
     
-    function() public payable refillLimit(26 ether) {}
+    function() public payable balanceMaximum(26 ether) {}
 }
