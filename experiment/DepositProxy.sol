d431de0623e62f320e365921f7e6518254460913
8kb <mendel@zzzlab.com>
2018-03-10 22:40:10 +0200

    Create DepositProxy.sol

---
+++ experiment/DepositProxy.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.18;
+import "./DepositProxy.sol";
+import "github.com/8kb/DryMoney/excange/Accounts.sol";
+
+contract ProxyFactory is ExchangeAccounts {
+    mapping(address => address) private depositProxys;
+
+    function createDepositAddress() public {
+        require(proxys[msg.sender] == address(0));
+        depositProxys[msg.sender] = new DepositProxy(msg.sender);
+    }
+    
+    function getDepositAddress() public returns(address) {
+        require(proxys[msg.sender] == address(0));
+        return depositProxys[msg.sender];
+    }
+    
+    function depositTo() payable public {
+        
+    }    
+    
+    function depositTokenTo() public {
+        
+    }
+}
+
+import "github.com/8kb/DryMoney/token/IErc20Basic.sol";
+
+contract DepositProxy {
+//    address internal owner;
+//    address internal target;
+    address public owner;
+    address public target;
+    function DepositProxy(address _owner) {
+        owner = _owner;
+        target = msg.sender;
+    }
+    
+    function() public payable {
+        ProxyFactory(target).depositTo.value(this.balance)(owner);
+    }
+    
+    function transferToken(address _token) public returns (bool) {
+        token = TokenIErc20Basic(_token);
+        uint256 value = token.balanceOf(this);
+        require(token.transfer(target, value));
+        require(ProxyFactory(target).depositTokenTo.(owner, value));
+        return true;
+    }
+}
