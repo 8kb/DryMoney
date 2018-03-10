01f747f044635c8d3333f8cbfd1123c62a8bfc02
8kb <mendel@zzzlab.com>
2018-03-10 23:47:33 +0200

    Create MinimumLimit.sol

---
+++ share/MinimumLimit.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.18;
+import "./IShared.sol";
+import "../lib/SafeMath.sol";
+
+contract ShareMinimumLimit is ShareIShared {
+    using LibSafeMath for uint256;
+    
+    uint256 public shareMinimumLimit;
+    
+    /**
+     * @dev Internal transfer tokens from one address to another
+     * @dev if adress is zero - mint or destroy tokens
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     */
+    function shareTransfer(address _from, address _to, uint256 _value) internal returns (bool) {
+        if(_from == address(0)) {
+            require(shareCount().add(_value) >= shareMinimumLimit);
+        }
+        return super.shareTransfer(_from, _to, _value);
+    }
+}
