77996a43e9c7cea733b55c3cb219d5cc0cf9ea48
8kb <mendel@zzzlab.com>
2018-04-02 23:16:14 +0300

    upd

--- balance/WithdrawByShareholder.sol
+++ balance/WithdrawByShareholder.sol
@@ -15,8 +15,8 @@ contract BalanceWithdrawByShareholder is ShareIShared {
         require(shareCount() != 0);
 //        uint256 balansedShare = shareSize(msg.sender).mul(address(this).balance); 
 //        uint256 amount =  balansedShare.div(shareCount());
-        uint256 shareSize = shareSize(msg.sender); 
-        uint256 amount = address(this).balance.mulDiv(shareSize, shareCount());
+        uint256 senderShare = shareSize(msg.sender); 
+        uint256 amount = address(this).balance.mulDiv(senderShare, shareCount());
 
         require(amount != 0);
         shareTransfer(msg.sender, 0, shareSize(msg.sender));
