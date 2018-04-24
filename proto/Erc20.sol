8aa45ee0caf9c7a29db57c89f176979143302848
8kb <mendel@zzzlab.com>
2018-04-24 12:45:01 +0300

    Fuck!

--- proto/Erc20.sol
+++ proto/Erc20.sol
@@ -5,7 +5,7 @@ import "./Erc20Basic.sol";
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-contract ProtoErc20 is ContractErc20Basic {
+contract ProtoErc20 is ProtoErc20Basic {
     function allowance(address owner, address spender) public view returns (uint256);
     function transferFrom(address from, address to, uint256 value) public returns (bool);
     function approve(address spender, uint256 value) public returns (bool);
